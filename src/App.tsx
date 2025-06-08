373a9a3cfd784c454a4f9d655885d4cf638976f2
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-08 13:54:46 -0500

    added setup for button and input field

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ function App() {
   return(<>
     <h1 className='text-center text-7xl font-bold'
     >Clash Royale Card Finder</h1>
+
+
+    <input className="font-bold"
+    ></input>
+
+    <button className="font-bold "
+    >search</button>
   
   </>)
 };
