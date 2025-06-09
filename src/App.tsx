77de87eaadf5ee294b82e55925c1577c792d4ab2
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-09 01:08:15 -0500

    added an input div to put control elements like the search button and search text box inside so i can center and overall have more control overe and fixed some bugs and am fixing styling issues and consistancies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 function App() {
   return(<>
-    <h1 id="MainH1" className='text-center text-8xl'
+    <h1 id="MainH1" className='text-center text-8xl text-shadow-lg/100 text-shadow-purple-950'
     >Clash Royale Card Finder</h1>
 
 
 
-    <div className="mt-8">
-
-
-
+    <div className="mt-8 inputDiv" >
       <input className="font-bold border-2 rounded"
       ></input>
 
