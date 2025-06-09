8a0fa3a50561e17ff9c00f4559e828492fe0ec35
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-09 01:52:01 -0500

    put both input functions inside the app function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import { useEffect } from "react"
 
-function typedFunc(){
-  console.log("typed character")
-};
-
-
-function clickedFunc(){
-};
-
-
-
 function App() {
+  function clickedFunc(){console.log("tests click")}
+
+  function typedFunc(){console.log("test type")}
 
 
+  
 return(<>
     <h1 id="MainH1" className='text-center text-8xl text-shadow-lg/100 text-shadow-purple-950'
     >Clash Royale Card Finder</h1>
@@ -21,10 +15,14 @@ return(<>
       <input className="border-2 h-9 w-35"
       onChange={typedFunc}
       ></input>
-
+  
+  
       <button className="border-2 h-9 w-20"
       onClick={clickedFunc}
-      >search</button>
-    
-    </div> </>) };
+      >search</button>  
+    </div>
+
+
+    </>) };
+
 export default App
