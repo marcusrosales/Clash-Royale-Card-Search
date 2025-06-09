f975b3ed746d2b2c5950b3dd5941d3bf290e395e
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-09 01:43:25 -0500

    added template functions for typing and button pressing to the two inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import { useState } from "react"
 import { useEffect } from "react"
 
+function typedFunc(){
+  console.log("typed character")
+};
+
+
+function clickedFunc(){
+};
+
+
 
 function App() {
-  return(<>
+
+
+return(<>
     <h1 id="MainH1" className='text-center text-8xl text-shadow-lg/100 text-shadow-purple-950'
     >Clash Royale Card Finder</h1>
     <div className="mt-8 inputDiv" >
       <input className="border-2 h-9 w-35"
+      onChange={typedFunc}
       ></input>
 
       <button className="border-2 h-9 w-20"
+      onClick={clickedFunc}
       >search</button>
     
     </div> </>) };
-
 export default App
-
-
