6d48e4ed2336e6edf70772a1007585d1ea070f67
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-09 01:26:32 -0500

    added spacing between buttons and inputfield and fixed more bugs

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,13 @@ function App() {
   return(<>
     <h1 id="MainH1" className='text-center text-8xl text-shadow-lg/100 text-shadow-purple-950'
     >Clash Royale Card Finder</h1>
-
-
-
     <div className="mt-8 inputDiv" >
-      <input className="font-bold border-2 rounded"
+      <input className="border-2 h-9 w-35"
       ></input>
 
-
-      <button className="font-bold border-2 rounded"
+      <button className="border-2 h-9 w-20"
       >search</button>
-    </div>
-  
-  </>)
-};
-
+    
+    </div> </>) };
 
 export default App
