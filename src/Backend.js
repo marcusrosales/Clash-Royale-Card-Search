a0be8f4a1b1d1b12187e43609c2210adc0c5de1f
marcusrosales <marcusrosalesofficial5@gmail.com>
2025-06-10 21:19:01 -0500

    got server sending json data for front end this took me soo long to even understand..

--- src/Backend.js
+++ src/Backend.js
@@ -1,22 +1,26 @@
 import express from 'express'
 import dotenv from "dotenv"
-
 dotenv.config();
+
 const token = process.env.REACT_APP_TOKEN
-
-
-
-
 const app = express()
-
-
 app.listen(3000)
 
-app.get("/", (req,res)=> {
-    res.send('hello')
+
+
+app.get("/CardRequest", async (req,res)=>{
+    const cards = await fetchCard()
+    res.json(cards)
 })
+    
 
+async function fetchCard() {
+    const response = await fetch('https://api.clashroyale.com/v1/cards/', {
+        headers: { Authorization: `Bearer ${token}`},
+    });
 
-console.log('testing ')
-
-
+    const data = await response.json();
+    const oneCard = data.items.slice(0,1);
+    return(oneCard)
+}
+console.log('server running')
