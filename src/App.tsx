5f6da62bfe37151bed7c17295711018b68144624
marcusrosales <marcusrosalesofficial5@gmail.com>
2025-06-10 22:25:07 -0500

    static image pops up after click function adding more data support and fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,46 @@
-
+import { useState } from "react"
 
 
 function App() {
+  
+  
+  const [cardImage, setCardImage] = useState('')
+  const [cardName, setCardName] = useState('')
+
 
   async function clickedFunc(){
     console.log('clicked button')
 
     const res = await fetch("http://localhost:3000/cardrequest")
     const data = await res.json()
-    console.log(data)
-  }
-  function typedFunc(){
-    console.log('typed character')
-  }
 
+    setCardImage(data[0].iconUrls.medium)
+    setCardName(data[0].name)
+
+  }
 return(<>
     <h1 id="MainH1" className='text-center text-8xl text-shadow-lg/100 text-shadow-purple-950'
     >Clash Royale Card Finder</h1>
     <div className="mt-8 inputDiv" >
       <input className="border-2 h-9 w-35"
-      onChange={typedFunc}
+      
       ></input>
   
   
       <button className="border-2 h-9 w-20"
       onClick={clickedFunc}
       >search</button>  
+    </div>
+
+
+
+
+    <div className="cardDiv">
+      <h1>{cardName}</h1>
+      <img src={cardImage}></img>
+
+
+
     </div>
 
 
